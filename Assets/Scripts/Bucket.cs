e67ff1527650e35b4d1675e4862e77347575fcb3
craigzeki <craig.zeki@gmail.com>
2022-03-30 01:05:29 +0100

    Comments added, code cleanup made

--- Assets/Scripts/Bucket.cs
+++ Assets/Scripts/Bucket.cs
@@ -6,25 +6,32 @@ using System.Linq;
 
 public class Bucket : MonoBehaviour
 {
+    //Debug items
     [SerializeField] TextMeshPro massText;
+    JointSpring mySpring = new JointSpring();
+    //End Debug items
+
+
+    //References to the key points of the scales
     [SerializeField] private Transform myForcePoint;
     [SerializeField] private Rigidbody myRB;
     [SerializeField] private HingeJoint myHinge;
 
+    //keep track of the total weight in this bucket
     [SerializeField] private float totalMass = 0;
 
+    //keep track of each of the objects in the bucket
     Dictionary<int, float> myWeighables = new Dictionary<int, float>();
 
-    JointSpring mySpring = new JointSpring();
-
-    // Start is called before the first frame update
     void Start()
     {
+        //Debug
         mySpring = myHinge.spring;
-
+        //End debug
     }
     public void ResetScales()
     {
+        //clear the list of objects and set the total weight to 0Kg
         myWeighables.Clear();
         totalMass = 0;
     }
@@ -32,11 +39,13 @@ public class Bucket : MonoBehaviour
     {
         //get the unioque ID of the weighable game object and use it as the key
         //this prevents double counting of mass as was sometimes seen when onTriggerExit / Enter failed to work / was janky
+        //Update of total mass will occur in FixedUpdate
         myWeighables[weighable.GetInstanceID()] = mass;
     }
 
     public void removeMass(GameObject weighable)
     {
+        //remove the weighable object from the list - mass will be removed on FixedUpdate
         myWeighables.Remove(weighable.GetInstanceID());
     }
 
@@ -44,10 +53,15 @@ public class Bucket : MonoBehaviour
     void FixedUpdate()
     {
         //using FixedUpdate for Physics calcs
+
+        //add up all the weights
         totalMass = myWeighables.Values.Sum();
 
+        //Debug Text
         massText.text = totalMass.ToString() + " Kg";
         //Vector3 myForce = Vector3.up * totalMass * Physics.gravity.y * Time.deltaTime;
+        
+        //Do not need Time.deltatime as force is being constantly applied
         Vector3 myForce = Vector3.up * totalMass * Physics.gravity.y;
 
        
@@ -55,8 +69,7 @@ public class Bucket : MonoBehaviour
         myHinge.spring = mySpring;
         //END DEBUG
         
-
-
+        //Add the force to the balance arm
         myRB.AddForceAtPosition(myForce, myForcePoint.position);
     }
 }
--- Assets/Scripts/WeighableObject.cs
+++ Assets/Scripts/WeighableObject.cs
@@ -10,18 +10,10 @@ using ZekstersLab.Helpers;
 [RequireComponent(typeof(CheckBounds))]
 public class WeighableObject : MonoBehaviour, iDragAndDrop
 {
-
+    //Debug
     //[SerializeField] TextMeshPro debugText;
+    //End Debug
 
-
-    //[SerializeField] private Collider myScales = null;
-    //private GameObject collidingWeighable = null;
-
-
-    //public Collider MyScales { get => myScales; set => myScales = value; }
-
-    //private bool scalesChanged = false;
-    //[SerializeField] private int numOfCollisions = 0;
 
     [SerializeField] GameObject groundMarkerPrefab;
     [SerializeField] GameObject groundMarker;
@@ -43,76 +35,77 @@ public class WeighableObject : MonoBehaviour, iDragAndDrop
     //Refactor vars
     [SerializeField] private float myMass;
     private Rigidbody myRB;
-    private GameObject _directBucket = null;
-    private GameObject _byRefBucket = null;
-    private GameObject _bucket = null;
+    private GameObject directBucket = null;
+    private GameObject byRefBucket = null;
+    private GameObject bucket = null;
     private Dictionary<int, GameObject> touchingWeighables = new Dictionary<int, GameObject>();
 
     public float MyMass { get => myMass; }
-    public GameObject Bucket { get => _bucket;}
+    public GameObject Bucket { get => bucket;}
     public bool IsGrounded { get => isGrounded;}
     int gap1 = 3;
     int gap2 = 1;
     private void Awake()
     {
+        //Setup references to components
         myRB = GetComponent<Rigidbody>();
         TryGetComponent<FixRotationToZero>(out myFixedRotator);
 
+        //guards and asserts
         Debug.Assert(myRB != null, "Weighable:Awake:Assert myRB cannot be null");
         Debug.Assert(groundMarkerPrefab != null, "Weighable:Awake:Assert groundMarkerPrefab cannot be null");
         Debug.Assert(cheveronPrefab != null, "Weighable:Awake:Assert cheveronPrefab cannot be null");
         Debug.Assert(cheveronSpawnPoint != null, "Weighable:Awake:Assert cheveronSpawnPoint cannot be null");
 
+        //assume we are on the ground for first start
+        //could be replaced with a mechanism to check this first
         setIsGrounded(false);
+
+        //create the ground marker and hide it for now (shown while moving the object around)
         groundMarker = Instantiate(groundMarkerPrefab);
-        
         groundMarker.SetActive(false);
 
+        //Cheveron template created here and then used to get theY size for later use and scaling
+        //Getting the ySize is done in Start() as this requires the CheckBounds script attached to the Cheveron prefab
+        //to be available and have executed once
         firstCheveron = CreateCheveron();
-        //cheveronYSize = firstCheveron.GetComponent<CheckBounds>().MyBoundsSize.y;
         firstCheveron.SetActive(false);
 
+        //Set this objects mass to the one configured in the rigid body
         myMass = myRB.mass;
     }
 
-    public GameObject CreateCheveron()
-    {
-        GameObject temp = Instantiate(cheveronPrefab);
-        temp.GetComponent<cheveron>().transmittingObject = this.gameObject;
-        temp.GetComponent<cheveron>().targetObject = groundMarker;
-        //temp.transform.position = cheveronSpawnPoint.position - new Vector3(0,temp.GetComponent<CheckBounds>().MyBoundsSize.y,0);
-        temp.transform.position = cheveronSpawnPoint.position;
-        //temp.GetComponent<cheveron>().SetMyScale();
-        Helpers.matchXSizeLockedAspectRatio(this.gameObject, temp, 0.75f);
-        temp.GetComponent<CheckBounds>().calcBoundsNow();
-
-        return temp;
-    }
-
-    // Start is called before the first frame update
+    //things done here cannot be done in awake as they require the object to finish instantiating and to populate data into properties so that it can be used here
     void Start()
     {
+        //set the initial y position to be used in the drag / drop mechanism
         dragStartPosY = transform.position.y;
-        
+
+        //get the cheveron y size for use in scaling calulations later on
+        //can remove the template cheveron then as no longer needed
         cheveronYSize = firstCheveron.GetComponent<CheckBounds>().MyBoundsSize.y;
         Destroy(firstCheveron);
     }
 
     private void OnEnable()
     {
-
+        //subscribe to the game manager OnGameStateChanged
         GameManager.Instance.OnGameStateChanged += GameStateChanged;
     }
 
     private void OnDisable()
     {
+        //unsubscribe from the game manager OnGameStateChanged, providing the game manager hasn't already been destroyed
         if (GameManager.Instance == null) { return; }
         GameManager.Instance.OnGameStateChanged -= GameStateChanged;
     }
 
     private void GameStateChanged(GameStates previousGameState, GameStates newGameState)
     {
-        //guard
+        //This gets fired when the Game Manager changes state
+        //Used here to ensure we reset the drag drop touch parameters for calculating height
+
+        //guard - must be in GameSelection mode
         if (newGameState != GameStates.GameSelection) { return; }
 
         //scales are now in position, set the y position for calculating movement
@@ -121,113 +114,90 @@ public class WeighableObject : MonoBehaviour, iDragAndDrop
 
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public GameObject CreateCheveron()
     {
-        //Old code
-        //if(scalesChanged)
-        //{
+        //This function will create a single cheveron and scale it based on the object it is being transmitted from
 
-        //    if(myScales != null)
-        //    {
-        //        Debug.Log(this.gameObject.name.ToString() + ": Scales Changed: " + myScales.name.ToString());
-        //        myScales.GetComponentInParent<Scales>().addMass(myMass, myScales);
-        //    }
-        //    else { Debug.Log(this.gameObject.name.ToString() + "Scales Null"); }
+        GameObject temp = Instantiate(cheveronPrefab);
+        temp.GetComponent<cheveron>().transmittingObject = this.gameObject;
+        temp.GetComponent<cheveron>().targetObject = groundMarker;
+        temp.transform.position = cheveronSpawnPoint.position;
 
-        //    scalesChanged = false;
-        //}
+        //calculate the scale to make the cheveron match 75% of this weighable objects scale
+        Helpers.matchXSizeLockedAspectRatio(this.gameObject, temp, 0.75f);
 
-        //Refactor code
+        //force a recalculate of the cheveron bounds
+        temp.GetComponent<CheckBounds>().calcBoundsNow();
 
-        
-
-       
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        //Old code
-        //numOfCollisions++;
-        //myRB.mass = 0;
-        //Debug.Log(this.gameObject.name.ToString() + ": Collision with: " + collision.gameObject.name.ToString());
-        //switch (collision.gameObject.tag)
-        //{
-        //    case "Scales":
-        //        scalesChanged = true;
-        //        //scales will do the hard part of detecting which collider and grabing the mass from this object
-        //        //just keep track of the fact that we are on the scales so that we do not pass our mass to other weighableobjects
-        //        break;
-
-        //    case "WeighableObject":
-        //        collidingWeighable = collision.gameObject;
-        //        if ((scalesChanged == false) && (myScales == null))
-        //        {
-        //            myScales = collision.gameObject.GetComponent<WeighableObject>().MyScales;
-        //            if (myScales != null)
-        //            {
-        //                myScales.GetComponentInParent<Scales>().addMass(myMass, myScales);
-        //                //scalesChanged = true;
-        //            }
-
-        //        }
-        //        break;
-        //}
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-
-        //if ((--numOfCollisions == 0) || (collision.gameObject.tag == "Scales"))
-        //{
-        //    myRB.mass = MyMass;
-        //    if (myScales != null)
-        //    {
-        //        myScales.GetComponentInParent<Scales>().removeMass(myMass, myScales);
-        //        myScales = null;
-        //        scalesChanged = true;
-        //    }
-
-        //}
-
-
+        return temp;
     }
 
     private void setIsGrounded(bool state)
     {
-        
+        //Set is grounded state and update the myFixedRotator state if it is attached
         isGrounded = state;
         if(myFixedRotator == null) { return; }
         myFixedRotator.enabled = isGrounded;
     }
    
-
     private void OnTriggerEnter(Collider other)
     {
+        //handle the various things we can trigger / be triggered by
         switch (other.tag)
         {
             case "Ground":
-                //reset the start pos
+                //reset the start pos for drag / drop
                 dragStartPosY = transform.position.y;
                 dragStartPos2D = new Vector2(transform.position.x, transform.position.z);
                 setIsGrounded(true);
                 break;
             case "Bucket":
-                _directBucket = other.gameObject;
+                //store the reference to the bucket we are touching
+                directBucket = other.gameObject;
+                //set this as my bucket - will override any other buckets that are set indirectly (if this was only previously touching a weighable object)
                 CheckAndSetBucket();
                 break;
             case "WeighableObject":
+                //touching a weighable object
+                //get the referebce to this weighables bucket (if it has one) and store it in the dictionary
                 touchingWeighables[other.gameObject.GetInstanceID()] = other.gameObject.GetComponent<WeighableObject>().Bucket;
+                //if there is no 'direct bucket' that we are touching then set the indirect bucket from the other weighable as ours for now
                 CheckAndSetBucket();
                 break;
         }
+    }
 
-        
+    private void OnTriggerExit(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Ground":
+                //set the start pos for drag / drop
+                dragStartPosY = transform.position.y;
+                dragStartPos2D = new Vector2(transform.position.x, transform.position.z);
+                setIsGrounded(false);
+                break;
+            case "Bucket":
+                //clear the direct bucket as no longer touching one
+                directBucket = null;
+                //revert to indirect bucket or no bucket
+                CheckAndSetBucket(other.gameObject);
+                break;
+            case "WeighableObject":
+                //remove the weighable we were touching from the list of weighables
+                touchingWeighables.Remove(other.gameObject.GetInstanceID());
+                //recheck if we have any bucket and if not, remove the mass from the previously linked bucket
+                CheckAndSetBucket(other.gameObject.GetComponent<WeighableObject>().Bucket);
+                break;
+        }
+
+
     }
 
     private void OnTriggerStay(Collider other)
     {
+        //used to enforce isGrounded
         switch (other.tag)
         {
             case "Ground":
@@ -240,39 +210,50 @@ public class WeighableObject : MonoBehaviour, iDragAndDrop
 
     public bool CheckAndSetBucket()
     {
-        if (_directBucket != null)
+        //This function checks to see which buckets, indirect or direct are available and
+        //sets the one with highest priority (direct has highest)
+
+        if (directBucket != null)
         {
-            //use these
-            _bucket = _directBucket;
+            //highest priority - do this check first and exit function ASAP
+            bucket = directBucket;
         }
         else if (touchingWeighables.Count > 0)
         {
-            _byRefBucket = null;
+            //check for a bucket in the weighables that we are touching
+            //as each bucket is far apart, we will assume that all weighables in
+            //this list will contain the same bucket, and therefore can exit
+            //the ForEach loop as soon as one is found
+            byRefBucket = null;
             //backups
             foreach (KeyValuePair<int, GameObject> bucket in touchingWeighables)
             {
                 if (bucket.Value != null)
                 {
-                    _byRefBucket = bucket.Value;
+                    byRefBucket = bucket.Value;
                     
                     break;
                 }
             }
-            _bucket = _byRefBucket;
+            bucket = byRefBucket;
         }
         else
         {
-            _bucket = null;
+            //no bucket available
+            bucket = null;
         }
 
-        if (_bucket != null)
+        //if we have a bucket, set this objects weight to 0 (or as close as we can get within Unity physics)
+        //Add the mass to the bucket
+        if (bucket != null)
         {
             myRB.mass = 0.01f;
-            _bucket.GetComponent<Bucket>().addMass(this.gameObject, MyMass);
+            bucket.GetComponent<Bucket>().addMass(this.gameObject, MyMass);
             return true;
         }
         else
         {
+            //we are no longer connected to a bucket, restore the mass of the object
             myRB.mass = MyMass;
             return false;
         }
@@ -280,44 +261,29 @@ public class WeighableObject : MonoBehaviour, iDragAndDrop
 
     public bool CheckAndSetBucket(GameObject exitingColliderGameObject)
     {
+        //This Method Overload provides a way to indicate that the function was called from the 'OnTriggerExit' function
+        //It takes an additinal member which we can use to remove our weight from the bucket
         if(!CheckAndSetBucket())
         {
+            //call the base function first and if no other buckets exist...
             if (exitingColliderGameObject != null)
             {
+                //if the member is set, get its bucket and remove our mass
                 exitingColliderGameObject.GetComponent<Bucket>().removeMass(this.gameObject);
             }
             
+            //return false
             return false;
         }
         else
         {
+            //return true as the base function would have also returned true in this case
             return true;
         }
     }
 
 
-
-    private void OnTriggerExit(Collider other)
-    {
-        switch (other.tag)
-        {
-            case "Ground":
-                dragStartPosY = transform.position.y;
-                dragStartPos2D = new Vector2(transform.position.x, transform.position.z);
-                setIsGrounded(false);
-                break;
-            case "Bucket":
-                _directBucket = null;
-                CheckAndSetBucket(other.gameObject);
-                break;
-            case "WeighableObject":
-                touchingWeighables.Remove(other.gameObject.GetInstanceID());
-                CheckAndSetBucket(other.gameObject.GetComponent<WeighableObject>().Bucket);
-                break;
-        }
-
-        
-    }
+    //Touch Controls and associated (markers & cheverons) below this point
 
     public void OnStartDrag()
     {
@@ -471,4 +437,75 @@ public class WeighableObject : MonoBehaviour, iDragAndDrop
 
         return dropPoints[0];
     }
+
+
+    //Old code - left here so it can be seen what did not work!
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        //Old code
+        //if(scalesChanged)
+        //{
+
+        //    if(myScales != null)
+        //    {
+        //        Debug.Log(this.gameObject.name.ToString() + ": Scales Changed: " + myScales.name.ToString());
+        //        myScales.GetComponentInParent<Scales>().addMass(myMass, myScales);
+        //    }
+        //    else { Debug.Log(this.gameObject.name.ToString() + "Scales Null"); }
+
+        //    scalesChanged = false;
+        //}
+
+        //Refactor code
+
+    //}
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    //Old code
+    //    //numOfCollisions++;
+    //    //myRB.mass = 0;
+    //    //Debug.Log(this.gameObject.name.ToString() + ": Collision with: " + collision.gameObject.name.ToString());
+    //    //switch (collision.gameObject.tag)
+    //    //{
+    //    //    case "Scales":
+    //    //        scalesChanged = true;
+    //    //        //scales will do the hard part of detecting which collider and grabing the mass from this object
+    //    //        //just keep track of the fact that we are on the scales so that we do not pass our mass to other weighableobjects
+    //    //        break;
+
+    //    //    case "WeighableObject":
+    //    //        collidingWeighable = collision.gameObject;
+    //    //        if ((scalesChanged == false) && (myScales == null))
+    //    //        {
+    //    //            myScales = collision.gameObject.GetComponent<WeighableObject>().MyScales;
+    //    //            if (myScales != null)
+    //    //            {
+    //    //                myScales.GetComponentInParent<Scales>().addMass(myMass, myScales);
+    //    //                //scalesChanged = true;
+    //    //            }
+
+    //    //        }
+    //    //        break;
+    //    //}
+    //}
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+
+    //    //if ((--numOfCollisions == 0) || (collision.gameObject.tag == "Scales"))
+    //    //{
+    //    //    myRB.mass = MyMass;
+    //    //    if (myScales != null)
+    //    //    {
+    //    //        myScales.GetComponentInParent<Scales>().removeMass(myMass, myScales);
+    //    //        myScales = null;
+    //    //        scalesChanged = true;
+    //    //    }
+
+    //    //}
+
+    //}
 }
